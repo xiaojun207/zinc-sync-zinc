6b27ae2b24179e6a26410996e9778167364e6b24
name <email@example.com>
2022-09-09 10:56:48 +0800

    build dockerfile

--- deploy/build.sh
+++ deploy/build.sh
@@ -6,13 +6,8 @@ APP_NAME="zinc-sync-zinc"
 
 VERSION=$(git describe --tags `git rev-list --tags --max-count=1`)
 VERSION=${VERSION/v/} # eg.: 0.2.5
-DOCKER_BUILD_TAG="${VERSION}"
 
-docker build \
-  --build-arg VERSION="${VERSION}" \
-   -t ${DOCKER_BASE_REPO}/${APP_NAME}:${VERSION} \
-   -f deploy/Dockerfile .
-
-#docker tag ${DOCKER_BASE_REPO}/${APP_NAME}:${VERSION} ${DOCKER_BASE_REPO}/${APP_NAME}:latest
-#docker push ${DOCKER_BASE_REPO}/${APP_NAME}:${VERSION}
-#docker push ${DOCKER_BASE_REPO}/${APP_NAME}:latest
+docker build -t ${DOCKER_BASE_REPO}/${APP_NAME}:${VERSION} -f deploy/Dockerfile .
+docker tag ${DOCKER_BASE_REPO}/${APP_NAME}:${VERSION} ${DOCKER_BASE_REPO}/${APP_NAME}:latest
+docker push ${DOCKER_BASE_REPO}/${APP_NAME}:${VERSION}
+docker push ${DOCKER_BASE_REPO}/${APP_NAME}:latest
